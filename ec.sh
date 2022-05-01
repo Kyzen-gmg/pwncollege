07a2fd2c9bfff44a5e3a8f706878e2efc85abef1
root <root@kali>
2022-05-01 17:46:04 -0400

    ec.sh commit

---
+++ ec.sh
@@ -0,0 +1,7 @@
+# AutoRecon
+cd /opt
+git clone https://github.com/Tib3rius/AutoRecon.git
+sudo apt install seclists curl enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
+
+# Evil-winrm
+gem install evil-winrm
