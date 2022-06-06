6905390abc6968b4e0b65845ca2c1a48bf516e6f
kmsi <keicaste17@gmail.com>
2022-06-06 17:31:57 -0400

    config

---
+++ lang_c/ex_decision_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main () {
+
+    /* define local variable */
+    int a = 100;
+
+    /* check the boolean condition */
+    if( a < 20 ) {
+        /* if condition is true then print the following */
+        printf("Value of a is less than 20\n");
+    } else {
+        /* if condition is false then print the following */
+        printf("Value of a is not less than 20\n");
+    }
+
+    printf("Value of a is : %d\n", a);
+
+    return 0;
+}
