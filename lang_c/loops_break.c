91ac041b9c97aacebd162a42aa8fea1e76c55791
kmsi <keicaste17@gmail.com>
2022-06-07 23:12:48 -0400

    upd

---
+++ lang_c/loops_break.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+
+    while( a < 20 ) {
+
+        printf("Value of a: %d\n", a);
+        a++;
+
+        if( a > 15) {
+            break;
+        }
+    }
+
+
+    return 0;
+}
